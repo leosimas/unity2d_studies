4ffd2065311b345749087833997c330b94113e69
Leonardo <leosimasgoncalves@gmail.com>
2019-01-02 22:32:51 -0300

    player movement

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public CharacterController2D controller;
+
+    private const float runSpeed = 40;
+
+    private float horizontalMove = 0;
+    private bool jump = false;
+    private bool crouch = false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+        if (Input.GetButtonDown("Jump")) jump = true;
+
+
+    }
+
+    void FixedUpdate()
+    {
+        controller.Move(horizontalMove * Time.fixedDeltaTime, crouch, jump);
+        jump = false;
+    }
+}
